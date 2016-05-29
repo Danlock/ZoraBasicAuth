aacde752553a51c8f1923949a62098653cee3da0
Dan <17@dlock.tech>
2016-05-29 18:33:21 -0300

    added mongodb, signup and login routes migrated and working

---
+++ routes.js
@@ -0,0 +1,140 @@
+'use strict';
+
+const Bcrypt = require('bcrypt');
+const Joi = require('joi');
+const Boom = require('boom');
+const uuid = require('node-uuid');
+
+// const users = {
+//     "john@doe.com": {
+//         password: '12',   // 'secret'
+//         name: 'John Doe',
+//         fname: 'John',
+//         id: '2133d32a'
+//     }
+// };
+
+const getUser = function (email,pw) {
+
+
+
+    return [1,2,email,header];
+}
+
+
+exports.register = function (server, options, next) {
+    const db = server.app.db;
+    const users = db.collection('users');
+
+    server.auth.strategy('simple', 'basic', { validateFunc: 
+        function (request, username, password, callback) {
+
+        const user = users.findOne({
+            email: username,
+            password: password
+        }, (err,doc) => {
+            if (err || !doc) {
+                return callback(null, false);
+            }
+
+            callback(null,true,{fname: doc.fname,lname: doc.lname,email:username,password:doc.password});
+        });
+        // if (!user || password !== user.password) {
+
+        //     return callback(null, false);
+        // }
+     
+        // callback(null,true,{fname:user.fname,lname:user.lname,email:username});
+
+        // Bcrypt.compare(password, user.password, (err, isValid) => {
+        //     callback(err, isValid, [user.fname,user.lname,user.email]);
+        // });
+        }
+    });
+
+    // Accepts: name, email, password.
+    //Stores: first_name, last_name, email, password
+    //responds: first_name, last_name, password or error 
+    server.route({
+        method: 'POST',
+        path: '/api/signup',
+        config: {
+            validate: {
+                payload: {
+                    name: Joi.string().min(2).max(60).required(),
+                    email: Joi.string().email().required(),
+                    password: Joi.string().min(1).max(200).required()
+                }
+            },
+
+            handler: function (request, reply) {
+                var name = request.payload.name;
+                var index = name.indexOf(" ");
+                var fname = name.substr(0, index);
+                var lname = name.substr(index+1);
+
+                var user = {_id: uuid.v1(),fname:fname,lname:lname,name:name,password:request.payload.password,email:request.payload.email};
+
+                users.save(user, (err,result) => {
+                    if (err) {
+                        return reply(Boom.badData('Internal MongoDB error!', err));
+                    } else {
+                        reply(user);
+                    }
+
+                });
+            }
+        }
+    });
+
+    //Accepts: email, password
+    //Returns: first_name, last_name, email, basic auth header or error 
+    server.route({
+        method: 'GET',
+        path: '/api/login',
+        config: {
+            auth: 'simple',
+            handler: function (request, reply) {
+                var header = 'Basic ' 
+                    + (new Buffer(request.auth.credentials.email + ':' 
+                    + request.auth.credentials.password, 'utf8'))
+                    .toString('base64');
+
+                var user = [request.auth.credentials.fname,
+                    request.auth.credentials.lname,
+                    request.auth.credentials.email,header]
+
+                reply(user);
+                
+            }
+        }
+    });
+
+    //Accepts: oldPass, newPass
+    //responds: 200, or error 
+    server.route({
+        method: 'POST',
+        path: '/api/reset/password',
+       config: {
+            validate: {
+                payload: {
+                    oldPass: Joi.string().min(1).max(200).required(),
+                    newPass: Joi.string().min(1).max(200).required()
+                }
+            },
+            auth: {strategy : 'simple'},
+            handler: function (request, reply) {
+                var success = reset(request.payload.oldPass,request.payload.newPass);
+
+                if (success) reply("Success").code(200);
+                else reply(Boom.unauthorized("Bad password!"))
+            }
+        }
+    });
+
+    return next();
+};
+
+exports.register.attributes = {
+    name: 'routes'
+}
--- server.js
+++ server.js
@@ -1,104 +1,21 @@
 'use strict';
 
-const Bcrypt = require('bcrypt');
-const Joi = require('joi');
 const Hapi = require('hapi');
 const Basic = require('hapi-auth-basic');
-const Boom = require('boom');
+const mongojs = require('mongojs');
+const Routes = require('./routes.js');
 
-const server = new Hapi.Server();
+const server = new Hapi.Server({ debug: { request: ['error'] } });
 server.connection({ port: 3000 });
+server.app.db = mongojs('hapi-rest-mongo'); 
 
-const users = {
-    john: {
-        username: 'john',
-        password: '12',   // 'secret'
-        name: 'John Doe',
-        fname: 'John',
-        lname: 'Doe',
-        email: 'john@doe.com',
-        id: '2133d32a'
-    }
-};
-
-const validate = function (request, username, password, callback) {
-    const user = users[username];
-    if (!user) {
-        return callback(null, false);
-    }
-   // callback(null,password === user.password ,{ id: user.id, name: user.name });
-    Bcrypt.compare(password, user.password, (err, isValid) => {
-        callback(err, isValid, { id: user.id, name: user.name });
-    });
-};
-
-const storeSignup = function (name, email, pw) {
-    var index = name.indexOf(" ");
-    var fname = name.substr(0, index);
-    var lname = name.substr(index+1);
-
-    //TODO: store in Postgres database
-
-    return [fname,lname,pw];
-}
 
 //Authentication
 
-server.register(Basic, (err) => {
-    server.auth.strategy('simple', 'basic', { validateFunc: validate });
-
-    // Accepts: name, email, password.
-    //Stores: first_name, last_name, email, password
-    //responds: first_name, last_name, password or error 
-    server.route({
-        method: 'POST',
-        path: '/api/signup',
-        config: {
-            validate: {
-                payload: {
-                    name: Joi.string().min(2).max(60).required(),
-                    email: Joi.string().email().required(),
-                    password: Joi.string().min(2).max(200).required()
-                }
-            },
-
-            handler: function (request, reply) {
-                var user = storeSignup(request.payload.name, request.payload.email,
-                    request.payload.password);
-
-                if (user) reply(user);
-                else reply(Boom.unauthorized("Bad email or password!"));
-
-                // reply('hullo, ' + request.auth.credentials.name);
-            }
-        }
-    });
-
-    //Accepts: email, password
-    //Returns: first_name, last_name, email, basic auth header or error 
-    server.route({
-        method: 'POST',
-        path: '/api/login',
-        config: {
-            auth: 'simple',
-            handler: function (request, reply) {
-                reply('hullo, ' + request.auth.credentials.name);
-            }
-        }
-    });
-
-    //Accepts: oldPass, newPass
-    //responds: 200, or error 
-    server.route({
-        method: 'GET',
-        path: '/api/reset/password',
-       config: {
-            auth: {strategy : 'simple'},
-            handler: function (request, reply) {
-                reply("Success, this should be a HTTP 200");
-            }
-        }
-    });
+server.register([
+    Basic,
+    Routes
+    ], (err) => {
 
         server.start(() => {
         console.log('server running at: ' + server.info.uri);
